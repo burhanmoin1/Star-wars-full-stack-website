115f2d9e9366d23d69079c2bd164b845cb058607
Burhan <m.burhanmoin1@gmail.com>
2023-08-22 08:33:39 -0700

    Merged changes from remote

--- webapp/webapp/templates/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var popupThreshold = $('.scrolling-text').offset().top; // Set the threshold for when to show the popup
-
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= popupThreshold) {
-            $('#navbarPopup').modal('show'); // Show the modal popup
-        }
-    });
-});
